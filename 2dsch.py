578d3da94dde1e84dfa5e82e07ae229e66aeddb3
tugberko <67656059+tugberko@users.noreply.github.com>
2021-11-02 13:57:50 +0300

    Update 2dsch.py

--- 2dsch.py
+++ 2dsch.py
@@ -8,18 +8,20 @@ import datetime
 import scipy.signal
 
 
-
-
 # Simulation Parameters
 
-dt = 1e-2   # Temporal seperation
+dt = 8e-2   # Temporal seperation
 
-nx = 40# Spatial grid points
-ny = 40
+# Spatial grid points
+nx = 60
+ny = 60
 
 Lx = 10    # Spatial size, symmetric with respect to x=0
-Ly = 10    # Spatial size, symmetric with respect to x=0
+Ly = 10   # Spatial size, symmetric with respect to x=0
 
+alpha=1
+kx = Lx/200
+ky = alpha * kx
 
 # Derived Simulation Parameters
 
@@ -30,6 +32,8 @@ x = np.linspace(-0.5*Lx, 0.5*Lx, nx)
 y = np.linspace(-0.5*Ly, 0.5*Ly, ny)
 
 
+# This function takes size (n) and seperation (dx) as Parameters
+# and produces a discrete 1D Laplacian matrix.
 def laplacian1D(size, separation):
     result = np.zeros((size, size))
     for i in range(size):
@@ -39,33 +43,55 @@ def laplacian1D(size, separation):
         result[i+1][i]=1
     return (-1/(2*(separation**2))) * result
 
+# This function creates a discrete 2D Laplacian matrix by taking kronecker sum
+# of two discrete 1D Laplacian matrix.
 def laplacian2D():
     Dxx = laplacian1D(nx, dx)
     Dyy = laplacian1D(ny, dy)
 
     return sp.kron(sp.eye(ny), Dxx) + sp.kron(Dyy,sp.eye(nx))
 
-def Potential(posx, posy, alpha):
+# This function calculates the anisotropic potential energy at specified position.
+# alpha is defined in such a way that   k_y = alpha * k_x   .
+def Potential(posx, posy):
     return 0.5*(posx**2) + 0.5*(alpha * (posy**2))
 
+# This function creates a discrete 2D potential energy matrix. It's compatible
+# with flattening with row priority.
 def VMatrix():
     result = np.zeros(( x.size*y.size , x.size*y.size ))
     cursor = 0
-    for i in range(x.size):
-        for j in range(y.size):
-            result[cursor][cursor] = Potential(x[i] , y[j], 64)
+    for i in range(y.size):
+        for j in range(x.size):
+            result[cursor][cursor] = Potential(x[j] , y[i])
             cursor += 1
 
     return result
 
+# This function creates a discrete 2D kinetic energy matrix.
 def TMatrix():
     return -0.5 * laplacian2D().toarray()
 
-
+# This function creates a discrete 2D Hamiltonian matrix.
 def HMatrix():
     return VMatrix() + TMatrix()
 
+def SquareSum2D(some_psi):
+    sum = 0
+    for i in range(len(some_psi)):
+        for j in range(len(some_psi[i])):
+            sum += some_psi[i][j] * np.conj(some_psi[i][j])
 
+    return sum
+
+def Normalize2D(some_psi):
+    scale_factor = 1/SquareSum2D(some_psi)
+    return scale_factor * some_psi
+
+
+# This function finds the specified order of excited state with respect to
+# given Hamiltonian. (order = 0 for groundstate, order = 1 for first excited
+# state vice versa.)
 def ExcitedState2d(order):
     H = HMatrix()
     val,vec=la.eig(H)
@@ -73,14 +99,64 @@ def ExcitedState2d(order):
 
     psi = vec[:,z[order]]
 
-    return psi.reshape(ny, nx)
+    return Normalize2D(psi.reshape(ny, nx))
 
 
-X, Y = np.meshgrid(x, y)
+def IdentityMatrix():
+    I = np.zeros((x.size*y.size, x.size*y.size))
+    for i in range(x.size*y.size):
+        I[i][i] = 1
+    return I
 
-plt.contourf(X,Y, np.abs(ExcitedState2d(0))**2,256, cmap='RdGy')
-plt.colorbar()
-plt.xlabel('x')
-plt.ylabel('y')
-plt.tight_layout()
-plt.show()
+def LHS():
+    first_term = IdentityMatrix()
+    second_term = (1j*HMatrix()*dt)/(2)
+
+    return first_term + second_term
+
+def RHS():
+    first_term = IdentityMatrix()
+    second_term = (1j*HMatrix()*dt)/(2)
+
+    return first_term - second_term
+
+
+
+
+LHS = LHS()
+RHS = RHS()
+InverseOfLHS = np.linalg.inv(LHS)
+
+
+
+# U is the time evolution matrix. To keep things time efficient,
+# I initialize this matrix once and use it as necessary.
+U = np.matmul(InverseOfLHS, RHS)
+
+
+
+# This function applies time evolution to a given wavefunction.
+def Evolve(some_psi):
+    return np.matmul(U, np.ndarray.flatten(some_psi)).reshape(ny,nx)
+
+
+def InitialState():
+    return ExcitedState2d(0) * np.exp(1j*kx*x) * np.exp(1j*ky*x)
+
+
+timesteps = 100
+
+psi = InitialState()
+
+for i in range(timesteps):
+    print('Timestep: ' + str(i))
+    plt.contourf(x,y, np.abs(psi)**2,256, cmap='RdYlBu')
+    plt.grid()
+    plt.colorbar()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.tight_layout()
+    plt.savefig('new' + str(i) +'.png')
+    plt.clf()
+
+    psi = Evolve(psi)
