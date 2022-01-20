70fb23d19229eaa114d8a947b5601fd877c30113
tugberko <67656059+tugberko@users.noreply.github.com>
2022-01-20 20:31:34 +0300

    Update 1dsch.py

--- 1dsch.py
+++ 1dsch.py
@@ -1,253 +1,180 @@
 #!/usr/bin/python
-import scipy.sparse as sp
+import scipy.sparse
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.linalg as la
 import math
 import datetime
-import scipy.signal
+from scipy.fftpack import fft, ifft, dct, idct, dst, idst, fftshift, fftfreq
+from numpy import linspace, zeros, array, pi, sin, cos, exp, arange, matmul, abs, conj, real, convolve, ones
 
+plt.rcParams['figure.figsize'] = 16, 9
+
+filename = 'splitstep.dat'
 
 # Simulation Parameters
 
-dt = 1e-2   # Temporal seperation
+dt = 0.02   # Temporal seperation
 
-# Spatial grid points
-nx = 31
-ny = 31
+halfdt = dt*0.5
 
-Lx = 60   # Spatial size, symmetric with respect to x=0
-Ly = 60   # Spatial size, symmetric with respect to x=0
+N = 2**8
 
-alpha=1
+L = 3 * pi
 
-kx = 1
-ky = 1
+dx = L/N
 
-# Derived Simulation Parameters
+x=arange(-L/2,L/2,dx)
 
-dx = Lx/(nx-1) # Spatial seperation
-dy = Ly/(ny-1) # Spatial seperation
 
-x = np.linspace(-0.5*Lx, 0.5*Lx, nx)
-y = np.linspace(-0.5*Ly, 0.5*Ly, ny)
+def GenerateMomentumSpace():
+    dk = (2*pi/L)
+    k = zeros(N)
+    if ((N%2)==0):
+        #-even number
+        for i in range(1,N//2):
+            k[i]=i
+            k[N-i]=-i
+    else:
+        #-odd number
+        for i in range(1,(N-1)//2):
+            k[i]=i
+            k[N-i]=-i
+
+    return dk * k
+
+k = GenerateMomentumSpace()
+
+ksq = k**2
+
 
 
 # This function takes size (n) and seperation (dx) as Parameters
 # and produces a discrete 1D Laplacian matrix.
-def laplacian1D(size, separation):
-    result = np.zeros((size, size))
-    for i in range(size):
+def laplacian1D():
+    result = np.zeros((N, N))
+    for i in range(N):
         result[i][i]=-2
-    for i in range(size-1):
+    for i in range(N-1):
         result[i][i+1]=1
         result[i+1][i]=1
-    return 1/(separation**2) * result
+    return 1/(dx**2) * result
 
-# This function creates a discrete 2D Laplacian matrix by taking kronecker sum
-# of two discrete 1D Laplacian matrix.
-def laplacian2D():
-    Dxx = laplacian1D(nx, dx)
-    Dyy = laplacian1D(ny, dy)
-
-    return sp.kron(sp.eye(ny), Dxx) + sp.kron(Dyy,sp.eye(nx))
-
-# This function calculates the anisotropic potential energy at specified position.
-# alpha is defined in such a way that force constant omega_y^2 = alpha * omega_x^2   .
-def Potential(posx, posy):
-    return 0.5*(posx**2) + 0.5*(alpha * (posy**2))
-
-# This function creates a discrete 2D potential energy matrix. It's compatible
-# with flattening with row priority.
-def VMatrix():
-    result = np.zeros(( x.size*y.size , x.size*y.size ))
-    cursor = 0
-    for i in range(y.size):
-        for j in range(x.size):
-            result[cursor][cursor] = Potential(x[j] , y[i])
-            cursor += 1
-
-    return result
-
-
-
-
-
-# This function creates a discrete 2D kinetic energy matrix.
+# This function outputs a matrix representing the kinetic
+# energy part of the Hamiltonian (Eqn.13).
 def TMatrix():
-    return -0.5 * laplacian2D().toarray()
+    return -0.5 * laplacian1D()
 
-# This function creates a discrete 2D Hamiltonian matrix.
+
+# This function calculates the potential energy at specified position.
+def Potential():
+    pot = np.zeros_like(x)
+    for i in range(N):
+        pot[i] = 0.5*(x[i]**2)
+    return pot
+
+
+# This function outputs a matrix representing the potential
+# energy part of the Hamiltonian (Eqn.13).
+def VMatrix():
+    return np.diag(Potential())
+
+# This function creates a discrete 1D kinetic energy matrix.
+def TMatrix():
+    return -0.5 * laplacian1D()
+
 def HMatrix():
-    return VMatrix() + TMatrix()
+    return TMatrix() + VMatrix()
 
-def SquareSum2D(some_psi):
-    sum = 0
-    for i in range(len(some_psi)):
-        for j in range(len(some_psi[i])):
-            sum += some_psi[i][j] * np.conj(some_psi[i][j]) * dx * dy
-
-    return sum
+def InitializeCrankNicholsonU():
+    lhs = la.expm(1j * HMatrix() * halfdt)
+    rhs = la.expm(-1j * HMatrix() * halfdt)
+    return np.matmul(la.inv(lhs) , rhs)
 
 
-# This function normalizes the given 2D wave.
-def Normalize2D(some_psi):
-    scale_factor = np.sqrt(1/SquareSum2D(some_psi))
-    return scale_factor * some_psi
+UCN = InitializeCrankNicholsonU()
 
-
-# This function finds the specified order of excited state with respect to
-# given Hamiltonian. (order = 0 for groundstate, order = 1 for first excited
-# state vice versa.)
-def ExcitedStateNumerical2D(order):
-    H = HMatrix()
-    val,vec=la.eig(H)
-    z = np.argsort(val)
-
-    psi = vec[:,z[order]]
-
-    return Normalize2D(psi.reshape(ny, nx))
+def EvolveCrankNicholson(some_psi):
+    return np.matmul(UCN, some_psi)
 
 
 
-# This function returns the analytical solution to the ground state
-def GroundStateExact2D():
-    result = np.zeros((ny, nx))
+V = VMatrix()
 
-    for i in range(ny):
-        for j in range(nx):
-            currentx = x[j]
-            currenty = y[i]
-            psi = (alpha**(0.125))*(np.exp(-0.5*(currentx**2)))*((np.exp(-0.5*(alpha**0.5)*(currenty**2))))
-            result[i][j] = psi
+def EvolveSplitStep(some_psi):
+    # Refer to: https://www.overleaf.com/7461894969pxkgqkzvmdws
 
-    return Normalize2D(result)
+    # First, neglect kinetic for half step, U1
+    psi = matmul( la.expm( -1j * halfdt * V) , some_psi  )
 
+    # Second, neglect potential for whole step, U2-hat in momentum space
+    psi_hat = fft(psi)
+    psi_hat = np.exp( -1j * ksq * dt / 2) * psi_hat
+    psi = ifft(psi_hat)
 
-# Name says it all
-def IdentityMatrix():
-    I = np.zeros((x.size*y.size, x.size*y.size))
-    for i in range(x.size*y.size):
-        I[i][i] = 1
-    return I
+    # Third, neglect kinetic for half more step, U3
+    psi = matmul( la.expm( -1j * halfdt * V) , psi  )
 
-def LHS():
-    first_term = IdentityMatrix()
-    second_term = (1j*HMatrix()*dt)/(2)
-
-    return first_term + second_term
-
-def RHS():
-    first_term = IdentityMatrix()
-    second_term = (1j*HMatrix()*dt)/(2)
-
-    return first_term - second_term
+    return psi
 
 
 
 
-LHS = LHS()
-RHS = RHS()
-InverseOfLHS = np.linalg.inv(LHS)
+# Exact solution of the coherent state
+def CoherentStateExact(t):
+    k=1
+    return ((1/np.pi)**(0.25)) * np.exp(   -0.5 * (x-k*np.sin(t))**2   ) * np.exp(1j*k*x*np.cos(t))
 
-
-
-# U is the time evolution matrix. To keep things time efficient,
-# I initialize this matrix once and use it as necessary.
-U = np.matmul(InverseOfLHS, RHS)
-
-
-
-# This function applies time evolution to a given wavefunction.
-def Evolve(some_psi):
-    return np.matmul(U, np.ndarray.flatten(some_psi)).reshape(ny,nx)
-
-
-# This function calculates the overlap between two 2D wavefunctions
-def Overlap2D(psi1, psi2):
+def Overlap(psi1, psi2):
     overlap = 0
     for i in range(x.size):
-        for j in range(y.size):
-            overlap += psi1[i][j] * np.conj(psi2[i][j]) * dx * dy
-    return np.abs(overlap)
-
-# This function calculates the overlap between two 1D wavefunctions
-def Overlap1D(psi1, psi2):
-    overlap = 0
-    for i in range(x.size):
-        overlap += psi1[i]*np.conj(psi2[i])*dx
+        overlap += psi1[i] * np.conj(psi2[i]) * dx
     return np.abs(overlap)
 
 
+psi_num_splitstep = CoherentStateExact(0)
+psi_num_cn = CoherentStateExact(0)
 
-
-def CoherentStateNumerical2D():
-    startingPoint = CoherentStateExact2D(0)
-    return startingPoint
-
-
-# Implementation of coherent state specified in the
-#
-# https://files.slack.com/files-pri/T5MM8M0CR-F02LPDYN02X/download/2021-11-09-2d_coherent_state.pdf
-#
-# with x_0=0
-def CoherentStateExact2D(t):
-    xinitial = 0
-    result = np.zeros((ny, nx), dtype=complex)
-    for i in range(ny):
-        for j in range(nx):
-            currentx = x[j]
-            currenty = y[i]
-            #A = (alpha**0.125)/(np.pi**0.5)
-            X = np.exp(  -0.5 * (currentx - xinitial * np.cos(t))**2    ) * np.exp(  -1j * xinitial * currentx * np.sin(t))
-            Y = np.exp(  -(0.5 * (alpha**0.5)) * (currenty - ky*np.sin(t*(alpha**0.5)))**2    ) * np.exp(  1j * ky * currenty * np.cos(t**(alpha**0.5)))
-            result[i][j] = X*Y
-
-    return Normalize2D(result)
-
-
-
-
-
-number_of_oscillations = 10
-
-terminateAt = number_of_oscillations * 2 * np.pi
+terminateAt = 20 * np.pi
 timesteps = math.ceil(terminateAt / dt)
-
-currentDate  = datetime.datetime.now()
-
-lx_over_kx = Lx/kx
-ly_over_ky = Ly/ky
-
-filename = str(nx) + 'x' + str(ny) + '.dat'
-
-with open(filename, 'a') as f:
-    f.write('# Simulation started at ' + str(currentDate) + '\n')
-    f.write('#\n')
-    f.write('# Simulation details: \n')
-    f.write('# Lx = ' + str(Lx) + ' \n')
-    f.write('# kx = ' + str(kx) + ' \n')
-    f.write('# Lx / kx = ' + str(lx_over_kx) + ' \n')
-
-    f.write('# Ly = ' + str(Ly) + ' \n')
-    f.write('# ky = ' + str(ky) + ' \n')
-    f.write('# Ly / ky = ' + str(ly_over_ky) + ' \n')
-    f.write('# dt = ' + str(dt) + ' s \n')
-    f.write('# dx = ' + str(dx) + ' \n')
-    f.write('# dy = ' + str(dy) + ' \n')
-    f.write('# Spatial grid points : ' + str(nx) + ' x ' + str(ny) + ' \n')
-    f.write('#\n')
-    f.write('# time\terror\n')
-
-psi_num = CoherentStateNumerical2D()
 
 for i in range(timesteps):
     currentTime = i*dt
-    print('Current time: ' + str(currentTime))
-    psi_exact = CoherentStateExact2D(currentTime)
-    error = 1 - Overlap2D(psi_num, psi_exact)
 
-    with open(filename, 'a') as f:
-        f.write(str(currentTime)+'\t'+str(error)+'\n')
+    psi_exact = CoherentStateExact(currentTime)
 
-    psi_num = Evolve(psi_num)
+    current_time_as_string = "{:.5f}".format(currentTime)
+    print('Current time: ' + current_time_as_string)
+
+
+    plt.suptitle('Grid Size: ' +str(N)+'\nTime: ' + current_time_as_string)
+    plt.subplot(1, 3, 1)
+    plt.plot(x, np.abs(psi_num_splitstep)**2)
+    error_ss = np.abs(1 - Overlap(psi_exact, psi_num_splitstep))
+    error_ss_string = "{:.3e}".format(error_ss)
+    plt.title('Split-step\nError: '+ error_ss_string)
+    plt.ylim(-0.02 , 0.6)
+    plt.grid()
+
+    plt.subplot(1, 3, 2)
+    plt.plot(x, np.abs(psi_num_cn)**2)
+    error_cn = np.abs(1 - Overlap(psi_exact, psi_num_cn))
+    error_cn_string = "{:.3e}".format(error_cn)
+    plt.title('Crank-Nicholson\nError: '+ error_cn_string)
+    plt.ylim(-0.02 , 0.6)
+    plt.grid()
+
+    plt.subplot(1, 3, 3)
+    plt.plot(x, np.abs(psi_exact)**2)
+    plt.title('Exact solution')
+    plt.ylim(-0.02 , 0.6)
+    plt.grid()
+
+    plt.tight_layout()
+    plt.savefig('visual'+str(i)+'.png', dpi=120)
+    plt.clf()
+
+
+
+    psi_num_splitstep = EvolveSplitStep(psi_num_splitstep)
+    psi_num_cn = EvolveCrankNicholson(psi_num_cn)
